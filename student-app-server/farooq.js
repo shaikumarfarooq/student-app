04d622e849d1e70ded383cdc1e885b8a0640b3a6
shaikumarfarooq <41444986+shaikumarfarooq@users.noreply.github.com>
2021-11-18 20:12:09 +0530

    Update farooq.js

--- student-app-server/farooq.js
+++ student-app-server/farooq.js
@@ -1,1 +1,35 @@
 
+import { app  } from "../index.js";
+import  getSchools from './db/school.js'
+
+
+export default function Api(){
+app.get("/school/get-all",(req,res)=>{
+    getSchools((school)=>{
+        const resutls = school.map(name=>{
+            return {name:name.NAME,address:{addressId:name.ADDRESSID ,houseNo:name.ADDRESS,street:name.STREET,town:name.TOWN ,district:name.TOWN ,state:name.STATE,country:name.COUNTRY ,is_address:name.IS_SCHOOL_ADDR  }}
+        })
+        res.send(resutls)
+    });
+
+
+});
+}
+
+
+
+
+import {getConnection} from './db.js'
+
+export default function getSchools(callbackFn){
+
+    const connection = getConnection();
+    connection.connect();
+
+    connection.query(`SELECT * FROM SCHOOL S, ADDRESS  A WHERE S.ADDRESSID = A.ADDRESSID`,(error,result,fields)=>{
+        if(error) throw error;
+        connection.end();
+        callbackFn(error ?[]: result)
+
+    })
+}
